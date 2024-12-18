586df00bb26bc850bde85c95538ff59fcf0a714b
DESKTOP-3JE9MET\Administrator <kkong3456@gmail.com>
2024-12-19 02:32:15 +0900

    stateful complete
    1:07:22

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:mitch_koko/pages/first_page.dart';
-import 'package:mitch_koko/pages/home_page.dart';
-import 'package:mitch_koko/pages/second_page.dart';
-import 'package:mitch_koko/pages/settings_page.dart';
+import 'package:mitch_koko/pages/counter_page.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
 
-// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<String> names = ['Mitch', 'Sharon', 'Beny'];
-
-    void userTapped() {
-      print('User tapped!');
-    }
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes:{
-        '/firstpage':(context)=>FirstPage(),
-        '/homepage':(context)=>HomePage(),
-        '/settingspage':(context)=>SettingsPage(),
-      }
+      home:CounterPage(),
     );
   }
 }
